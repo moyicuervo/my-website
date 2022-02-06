67fc4aa25bc2cc120c06f387878f0fb581ae62e2
moyano <moyanopablo@gmail.com>
2022-02-06 19:13:24 -0300

    https change talisman

--- main.py
+++ main.py
@@ -17,10 +17,11 @@ from flask_talisman import Talisman
 
 
 app = Flask(__name__)
+Talisman(app)
 app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY", "8BYkEfBA6O6donzWlSihBXox7C0sKR6b")
 ckeditor = CKEditor(app)
 Bootstrap(app)
-Talisman(app)
+
 gravatar = Gravatar(app, size=100, rating='g', default='retro', force_default=False, force_lower=False, use_ssl=False,
                     base_url=None)
 
